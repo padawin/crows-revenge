9844180cd8e976eb54e05f8dd4a17f391d326034
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-05-24 11:15:00 +0100

    speed of the crow increased

--- js/character.js
+++ js/character.js
@@ -87,7 +87,7 @@ loader.addModule('character', 'canvas', 'B', function (canvas, B) {
 		else if (this.state == CROW_STATES.RESTING && this.stamina >= this.maxStamina) {
 			this.state = CROW_STATES.MOVING;
 			this.target = ~~(Math.random() * (canvas.getWidth() - this.w));
-			this.speed.x = 2 * (this.x > this.target ? -1 : 1);
+			this.speed.x = 3 * (this.x > this.target ? -1 : 1);
 		}
 		else {
 			this.x += this.speed.x;
